920459eb6c4fbff13f0d1d90c8c0d65fb53d2b22
eky16 <86060467+eky16@users.noreply.github.com>
2026-04-20 10:17:14 +0700

    Update Googlemaps2.php

--- application/libraries/Googlemaps2.php
+++ application/libraries/Googlemaps2.php
@@ -20,7 +20,7 @@ class Googlemaps {
 	var $adsenseFormat				= 'HALF_BANNER';			// The format of the AdUnit
 	var $adsensePosition			= 'TOP_CENTER';				// The position of the AdUnit
 	var $adsensePublisherID			= '';						// Your Google AdSense publisher ID
-	var $apiKey						= 'AIzaSyACOmLyXKel5jB9TCcwUQiIzga0WHkcHpU'; 						// If you've got an API key you can use it by passing this parameter. Setup an API key here: https://code.google.com/apis/console
+	var $apiKey;						// If you've got an API key you can use it by passing this parameter. Setup an API key here: https://code.google.com/apis/console
 	var $backgroundColor			= '';						// A hex color value shown as the map background when tiles have not yet loaded as the user pans
 	var $bicyclingOverlay			= FALSE;					// If set to TRUE will overlay bicycling information (ie. bike paths and suggested routes) onto the map by default 
 	var $center						= "37.4419, -122.1419";		// Sets the default center location (lat/long co-ordinate or address) of the map. If defaulting to the users location set to "auto"
@@ -152,6 +152,11 @@ class Googlemaps {
 	var $placesAutocompleteBoundsMap= FALSE;					// An alternative to setting the SW and NE bounds is to use the bounds of the current viewport. If set to TRUE, the bounds will be set to the viewport of the visible map, even if dragged or zoomed
 	var $placesAutocompleteOnChange	= '';						// The JavaScript action to perform when a place is selected
 	
+	function __construct() {
+        $CI =& get_instance();
+        $this->apiKey = $CI->config->item('google_maps_api_key');
+    }
+
 	function Googlemaps($config = array())
 	{
 		if (count($config) > 0)
